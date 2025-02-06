ba027f54f9bb422117b4d8960aa7720de13343d2
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-02-06 20:09:26 +0500

    added -2 because there was a problem with list index out of range

--- week3/functions1/spygame.py
+++ week3/functions1/spygame.py
@@ -1,9 +1,10 @@
 def spy_game(nums):
-    for i in range(len(nums)-1):
+    for i in range(len(nums)-2):
         if nums[i] == 0 and  nums[i + 1] == 0 and nums[i+2] == 7:
             return True
     return False
 
 
-nums = list(map(int,(input("Enter integers: ").split())))
-print(spy_game(nums))
+if __name__ == "__main__":
+    nums = list(map(int,(input("Enter integers: ").split())))
+    print(spy_game(nums))
