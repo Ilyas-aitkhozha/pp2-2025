02aeb45f6cbd2debf896b11baa97a8ff31f9024d
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-02-06 20:13:32 +0500

    used some of the functions in this file

---
+++ week3/functions1/useofunctions.py
@@ -0,0 +1,10 @@
+import puzzle 
+import spygame
+import histogram
+puzzle.Solve(35,94)
+if (spygame.spy_game([3, 5, 4, 0, 0])):
+    print(True)
+else:
+    print(False)
+histogram.Histogram([1, 2, 5, 8])
+
