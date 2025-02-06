bce04a92677cac607ad9300da570b7fbc43bc033
Ilyas Aitkhozha <emilatanlike@gmail.com>
2025-02-06 20:14:52 +0500

    added more code for the 14 th exercise

--- week3/functions1/histogram.py
+++ week3/functions1/histogram.py
@@ -2,6 +2,6 @@ def Histogram(list):
     for x in list:
         print('*' * x)
 
-
-histogram = list(map(int,(input("Enter integers: ").split())))
-Histogram(histogram)
+if __name__ == "__main__":
+    histogram = list(map(int,(input("Enter integers: ").split())))
+    Histogram(histogram)
